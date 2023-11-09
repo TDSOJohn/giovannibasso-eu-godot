d74aac7e2f3c9fe308144d4815ea42f8eea44a1d
ARtbibaOfficial <giovanni.basso@icloud.com>
2023-11-09 20:26:28 +0100

    improving menu

--- PrimaryMenu.gd
+++ PrimaryMenu.gd
@@ -9,7 +9,27 @@ func _ready():
 	$MastodonOption.set_text_label("Mastodon")
 	$PixelfedOption.set_text_label("Pixelfed")
 
+func move_down():
+	cur_index += 1
+	if cur_index > 3:
+		cur_index = 0
+	focus_option()
+	print(cur_index)
+
+func move_up():
+	cur_index -= 1
+	if cur_index < 0:
+		cur_index = 3
+	focus_option()
+	print(cur_index)
+
+func focus_option():
+	if cur_index == 0:
+		$GithubOption.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("move_down"):
+		move_down()
+	if Input.is_action_just_pressed("move_up"):
+		move_up()
