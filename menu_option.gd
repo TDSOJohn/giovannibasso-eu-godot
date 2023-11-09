187f04042d61e3f38dcb3c10a26301289b193c47
ARtbibaOfficial <giovanni.basso@icloud.com>
2023-11-09 18:27:58 +0100

    first commit

---
+++ menu_option.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func set_text_label(text_in):
+	$Label.text = text_in
+
+func _on_button_pressed():
+	print("clicked!")
